aca453d831d51a9340ee136b2a2f2b10f8f10ab5
Hackle Wayne <hacklew@hotmail.com>
2016-09-24 19:56:37 +1200

    Types and init test

---
+++ Klondike/Dealer.fs
@@ -0,0 +1,33 @@
+﻿namespace Klondike
+
+[<RequireQualifiedAccess>]
+module Dealer =
+    type Card = 
+        | Two
+        | Three
+        | Four
+        | Five
+        | Six
+        | Seven
+        | Eight
+        | Nine
+        | Ten
+        | J
+        | Q
+        | K
+        | A
+    type Suit = Suit of Card list
+    type Pile = Pile of Card list
+    type Set = { Piles: Pile list }
+
+    let deal () = 
+        { Piles = List.init 7 (fun i -> Pile [] ) }
+
+module DealerTests =
+    open Xunit
+
+    [<Fact>]
+    let ``Each set should have 7 piles``() =
+        let set = Dealer.deal()
+        Assert.Equal(set.Piles|>List.length, 7)
+
