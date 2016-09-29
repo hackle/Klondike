5068ee068d1f873f3642a7e8af0ca05921d5da26
Hackle Wayne <hacklew@hotmail.com>
2016-09-30 01:55:58 +1300

    Move member methods to extensions

--- Klondike/GAme.fs
+++ Klondike/GAme.fs
@@ -37,24 +37,7 @@ module Game =
         { 
             Suit: Suit; 
             Cards: Card list 
-        } with
-        static member add (card: Card) foundation = 
-            let currentMaxVal = 
-                if foundation.Cards.IsEmpty then 
-                    EnumHelper.allValues<Face>()
-                    |> Seq.min
-                    |> int
-
-                else (int foundation.Cards.Head.Face)
-
-            let isIncrementBy1 = (int card.Face) = currentMaxVal + 1
-
-            let canAdd = 
-                card.Suit = foundation.Suit && isIncrementBy1
-
-            match canAdd with
-            | true -> { foundation with Cards = card :: foundation.Cards }
-            | false -> foundation
+        }
             
     type Foundations = 
         {
@@ -62,43 +45,16 @@ module Game =
             Club: Foundation;
             Heart: Foundation;
             Spade: Foundation;
-        } with
-        static member New () = 
-            { 
-                Diamond = { Suit = Diamond; Cards = [] };
-                Club = { Suit = Club; Cards = [] };
-                Heart = { Suit = Heart; Cards = [] };
-                Spade = { Suit = Spade; Cards = [] }
-            }
+        }
 
-        static member add (card: Card) foundations =
-            match card.Suit with
-            | Diamond -> { foundations with Diamond = foundations.Diamond |> Foundation.add card  }
-            | Club -> { foundations with Club = foundations.Club |> Foundation.add card }
-            | Heart -> { foundations with Heart = foundations.Heart |> Foundation.add card }
-            | Spade -> { foundations with Spade = foundations.Spade |> Foundation.add card }
-
-    type TableauPile = TableauPile of Card list with
+    type TableauPile = TableauPile of Card list
+    with 
         member this.Value = 
             let (TableauPile cards) = this
             cards
 
-        static member add (card: Card) (pile: TableauPile) =
-            let currentMaxValue = 
-                if pile.Value.IsEmpty then
-                    EnumHelper.allValues<Face>()
-                    |> Seq.max
-                    |> int
-                else pile.Value.Head.Face |> int
-
-            let canAdd = (int card.Face) = currentMaxValue - 1
-
-            match canAdd with
-            | false -> pile
-            | true -> TableauPile (card::pile.Value)
-
     type Set = { 
-        Tableau: TableauPile list; 
+        Tableau: TableauPile list;
         Stock: Card list;
         Discard: Card list;
         Foundations: Foundations
--- Klondike/Play.fs
+++ Klondike/Play.fs
@@ -1,17 +1,74 @@
 ﻿namespace Klondike
 
+module parts =
+    open Game
+
+    type Foundation with
+        static member add (card: Card) foundation = 
+            let currentMaxVal = 
+                if foundation.Cards.IsEmpty then 
+                    EnumHelper.allValues<Face>()
+                    |> Seq.min
+                    |> int
+
+                else (int foundation.Cards.Head.Face)
+
+            let isIncrementBy1 = (int card.Face) = currentMaxVal + 1
+
+            let canAdd = 
+                card.Suit = foundation.Suit && isIncrementBy1
+
+            match canAdd with
+            | true -> { foundation with Cards = card :: foundation.Cards }
+            | false -> foundation
+    
+    type Foundations with
+        static member New () = 
+            { 
+                Diamond = { Suit = Diamond; Cards = [] };
+                Club = { Suit = Club; Cards = [] };
+                Heart = { Suit = Heart; Cards = [] };
+                Spade = { Suit = Spade; Cards = [] }
+            }
+
+        static member add (card: Card) foundations =
+            match card.Suit with
+            | Diamond -> { foundations with Diamond = foundations.Diamond |> Foundation.add card  }
+            | Club -> { foundations with Club = foundations.Club |> Foundation.add card }
+            | Heart -> { foundations with Heart = foundations.Heart |> Foundation.add card }
+            | Spade -> { foundations with Spade = foundations.Spade |> Foundation.add card }
+
+    type TableauPile with
+        static member add (card: Card) (pile: TableauPile) =
+            let currentMaxValue = 
+                if pile.Value.IsEmpty then
+                    EnumHelper.allValues<Face>()
+                    |> Seq.max
+                    |> int
+                else pile.Value.Head.Face |> int
+
+            let canAdd = (int card.Face) = currentMaxValue - 1
+
+            match canAdd with
+            | false -> pile
+            | true -> TableauPile (card::pile.Value)
+
 [<RequireQualifiedAccess>]
 module Play =
     
     open Game
     open ListExtensions
+    
+    type Result<'s, 'f> =
+        | Success of 's
+        | Failure of 'f
 
     let deal () = 
         let allCardsShuffled = AllCards |> List.ofSeq |> List.shuffle
         let folder carry count =
             let (piles, rest) = carry
             let (take, leave) = List.splitAt count rest
-            take :: piles, leave
+            (TableauPile take) :: piles, leave
 
         let (tableau, undealt) =
             [ 7 .. -1 .. 1 ]
